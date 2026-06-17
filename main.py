02a6cb5824901733db2960718e96c3209c7e23ee
kishothsamuthiram <kishothsamuthiram@gmail.com>
2026-06-17 10:30:33 +0530

    Pipeline trigger test

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True)
 
 
-# Updated by Azure Pipeline practice - Kishoth
+# Updated by Azure Pipeline practice - azure pipeline testing---------------------
